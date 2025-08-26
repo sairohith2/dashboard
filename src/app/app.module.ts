ebfb932b8b6fdc9e1ba877f3d7734ea72f1b0236
sairohith2 <pulaparthirohith@gmail.com>
2025-08-26 12:29:48 -0400

    Removed a line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { WeatherForecastComponent } from './weather-forecast/weather-forecast.co
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-// Angular Material Modules
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
